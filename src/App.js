bb372189a9d1a194c47535ba3c884a97d472c28a
olgasuh444 <you@example.com>
2021-10-01 20:28:33 +0300

    TAM

--- src/App.js
+++ src/App.js
@@ -1,97 +1,43 @@
-import React, { useState } from 'react';
 import './App.css';
+import React, { useEffect } from "react";
+import ResponsiveDrawer from './views/NavigationUI'
+import { BrowserRouter, useLocation } from "react-router-dom";
+import RouterPages from './components/RouterPages'
+import Footer from './views/Footer';
+import Partners from './components/Partners';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
 
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
-
+  return null;
+}
 
 function App() {
-
-  const URI = 'https://jsonplaceholder.typicode.com/posts';
-
-  const fetchToDo = async () => {
-    const response = await fetch(URI);
-    const result = await response.json();
-    return result;
-  };
-
-  const [todos, setTodos] = useState([]);
-  document.addEventListener('DOMContentLoaded', () => {
-    fetchToDo().then(response => {
-      setTodos(response.slice(0, 4));
-    });
-  });
-  const completeTodo = index => {
-    const newTodos = [...todos];
-    newTodos[index].isCompleted = true;
-    setTodos(newTodos);
-  }
-  function Todo({ todo, index, completeTodo }) {
-    return (
-      <div className="todo"
-        style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}>
-        {todo.text}
-        <div>
-          <button onClick={() => completeTodo(index)}>Complete</button>
-          <button onClick={() => removeTodo(index)}>X</button>
-        </div>
-      </div>
-    )
-  }
-  const addTodo = text => {
-    const newTodos = [...todos, { text }];
-    setTodos(newTodos);
-  }
-
-  function TodoForm({ addTodo }) {
-    const [value, setValue] = useState('');
-
-    const handleSubmit = e => {
-      e.preventDefault();
-      if (!value) return;
-      addTodo(value);
-      setValue("");
-    }
-    return (
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          className="input"
-          value={value}
-          onChange={e => setValue(e.target.value)}
-        />
-      </form>
-    )
-  }
-
-  const removeTodo = index => {
-    const newTodos = [...todos];
-    newTodos.splice(index, 1);
-    setTodos(newTodos);
-  };
   return (
     <div className="App">
-
-      <div className="app">
-        <h1>ToDo List</h1>
-        <div className="todo-list">
-          {todos.map((todo, index) => (
-            <Todo
-              key={index}
-              index={index}
-              todo={todo}
-              // text={todo.title}
-
-              completeTodo={completeTodo}
-              removeTodo={removeTodo}
-            />
-          ))}
-          <TodoForm addTodo={addTodo} />
-
-        </div>
+      <div className="App-header">
+        <BrowserRouter>
+          <ScrollToTop />
+          <ResponsiveDrawer />
+          <div className="App-inner">
+            <RouterPages />
+            < Partners />
+          </div>
+          <Footer />
+        </BrowserRouter>
       </div>
     </div>
-  );
+  )
 }
 
 export default App;
+
+
+
+
+
+
