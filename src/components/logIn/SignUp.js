dfd4f4eaa9100a4f5c74095c1504ace0e5be44e5
olgasuh444 <you@example.com>
2021-10-04 21:28:47 +0300

    new commit

--- src/components/logIn/SignUp.js
+++ src/components/logIn/SignUp.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useContext } from 'react';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';
@@ -6,8 +6,8 @@ import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import { Controller, useForm } from "react-hook-form";
 import { makeStyles } from '@material-ui/core/styles';
-import './logIn/logIn.css'
-import { Context } from "../index";
+import './logIn.css'
+import { Context } from "../../index";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { TextField } from "@mui/material";
@@ -17,10 +17,8 @@ const useStyles = makeStyles((theme) => ({
     btn: {
         display: 'block',
         alignItems: 'center',
-        // marginBottom: 10,
         background: '#0C88A7',
         color: 'white',
-        /* text-transform: uppercase; */
         padding: '10px 5px',
         borderRadius: '4px',
         border: '1px solid #026E89',
@@ -42,100 +40,10 @@ const useStyles = makeStyles((theme) => ({
 
 }));
 
-
-function LogIn() {
+function SignUp() {
     const classes = useStyles();
     const [open, setOpen] = useState(false);
-    const { auth, firebase } = useContext(Context);
-
-    const { control, formState: { errors }, handleSubmit, setError } = useForm({
-        resolver: yupResolver(validationSchema)
-    });
-
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
-
-    const handleClose = () => {
-        setOpen(false);
-    };
-
-    const handleLogin = async ({ email, password }) => {
-        try {
-            const result = await firebase.auth().signInWithEmailAndPassword(email, password);
-            handleClose();
-
-        } catch (error) {
-            switch (error.code) {
-                case "auth/invalid-email":
-                case "auth/user-disabled":
-                case "auth/user-not-found": {
-                    setError('password', {
-                        message: error.message
-                    });
-                    break;
-                }
-                case "auth/wrong-password": {
-                    setError('email', {
-                        message: error.message
-                    });
-                    break;
-                }
-                default: {
-                    console.error(error);
-                    break;
-                }
-            }
-        }
-
-    }
-    return (
-        <div className="login">
-            <Button className={classes.btn} variant="outlined" color="primary"
-                onClick={handleClickOpen}>
-                Вхід
-            </Button>
-            <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-                <DialogTitle id="form-dialog-title">Вхід</DialogTitle>
-                <DialogContent>
-                    <DialogContentText>
-                        Авторизуйтесь для входу в систему
-                    </DialogContentText>
-                    <form onSubmit={handleSubmit(handleLogin)} className={classes.form}>
-                        <Controller
-                            control={control}
-                            name={'email'}
-                            render={
-                                ({ field }) => <TextField style={{ marginBottom: "10px" }} {...field} />
-                            }
-                        />
-
-                        <ErrorMessage name={'email'} errors={errors} render={({ message }) => <strong className={classes.formItem}>{message}</strong>} />
-
-                        <Controller
-                            control={control}
-                            name={'password'}
-                            render={
-                                ({ field }) => <TextField type={'password'} {...field} />
-                            }
-                        />
-
-                        <ErrorMessage name={'password'} errors={errors} render={({ message }) => <strong className={classes.formItem}>{message}</strong>} />
-
-                        <Button variant={'outlined'} color={'primary'} type={'submit'}>
-                            Вхід
-                        </Button>
-                    </form>
-                </DialogContent>
-            </Dialog>
-        </div>
-    )
-}
-
-function FormDialog() {
-    const classes = useStyles();
-    const [open, setOpen] = useState(false);
-    const { auth, firebase } = useContext(Context);
+    const { firebase } = useContext(Context);
 
     const { control, formState: { errors }, handleSubmit, setError } = useForm({
         resolver: yupResolver(validationSchema)
@@ -151,7 +59,7 @@ function FormDialog() {
 
     const submitHandler = async ({ email, password }) => {
         try {
-            const result = await firebase.auth().createUserWithEmailAndPassword(email, password);
+            await firebase.auth().createUserWithEmailAndPassword(email, password);
             handleClose();
 
         } catch (error) {
@@ -177,25 +85,6 @@ function FormDialog() {
 
     }
 
-    const [user, setUser] = useState('')
-
-    const handleLogOut = () => {
-        firebase.auth().signOut();
-    }
-    const authListener = () => {
-        firebase.auth().onAuthStateChanged((user) => {
-            if (user) {
-                setUser(user);
-                console.log(user);
-            } else {
-                setUser('');
-            }
-        })
-    }
-    useEffect(() => {
-        authListener();
-
-    }, [])
     return (
         <div style={{ display: 'flex' }}>
             <div className="signin">
@@ -238,10 +127,6 @@ function FormDialog() {
                     </DialogContent>
                 </Dialog>
             </div>
-
-
-
-            {user ? (<Button className={classes.btn} variant={'outlined'} color={'primary'} onClick={handleLogOut}>Вийти </Button>) : <LogIn />}
         </div >
     );
 }
@@ -251,4 +136,4 @@ const validationSchema = yup.object({
     password: yup.string().required()
 });
 
-export default FormDialog;
+export default SignUp;
